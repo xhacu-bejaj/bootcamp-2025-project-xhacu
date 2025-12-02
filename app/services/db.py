0e9bf2cdafb324d77d54807a87e1e7a55dce0eb9
xhacu <xhacubejaj@gmail.com>
2025-12-03 00:21:53 +0100

    add get_engine()

--- app/services/db.py
+++ app/services/db.py
@@ -1,20 +1,29 @@
 from typing import AsyncGenerator
-
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession, AsyncEngine
 
 
 from app.core.config import settings
 
 DATABASE_URL = settings.DATABASE_URL
 
-async_engine = create_async_engine(
-    DATABASE_URL,
-    echo=True,
-    future=True
-)
+_async_engine: AsyncEngine | None = None
+
+def get_engine() -> AsyncEngine:
+    """
+    Initializes and returns the singleton asynchronous SQLAlchemy engine.
+    This function handles the configuration once.
+    """
+    global _async_engine
+    if _async_engine is None:
+        _async_engine = create_async_engine(
+            DATABASE_URL,
+            echo=True, 
+            future=True
+        )
+    return _async_engine
 
 AsyncSessionLocal = async_sessionmaker(
-    bind=async_engine,
+    bind=get_engine(),
     class_=AsyncSession, 
     expire_on_commit=False 
 )
