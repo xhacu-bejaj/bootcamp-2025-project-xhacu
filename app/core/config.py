fe37f3a2776bc8e7664a877ba3664c161e2b1138
xhacu <xhacubejaj@gmail.com>
2025-12-03 00:09:57 +0100

    add ASYNC_DB in config.py, remove unnecessary imports in db.py

--- app/core/config.py
+++ app/core/config.py
@@ -2,11 +2,16 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     FILE_SNAPSHOT: bool = False
+    
     LOG_LEVEL: str = "INFO" 
+    
     DATABASE_URL: str = ''
+    ASYNC_DB: str = ''
+
     GOOGLE_API_KEY: str = ''
     OPENAI_API_KEY: str = ''
     DEEPSEEK_API_KEY: str = ''
+
     LOG_FILE_PATH: str = 'service-history.log'
     #MONGODB_CONNECTION_STRING: str = ''
     
--- app/services/db.py
+++ app/services/db.py
@@ -1,8 +1,7 @@
-import os
 from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-from sqlalchemy import text, inspect
+
 
 from app.core.config import settings
 
